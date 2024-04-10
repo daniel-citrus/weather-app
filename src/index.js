229362a2d106aabe187b8b03884e59f93a3d2c70
Daniel <dandcalvo@gmail.com>
2024-04-09 17:02:15 -0700

    getting weather

--- src/index.js
+++ src/index.js
@@ -2,46 +2,54 @@ import './style.scss';
 
 const API_KEY = 'dfe9055663544783911164832240904';
 
+/**
+ * Get object from the Weather API containing weather data
+ * @param {string} location
+ * @param {string} coords (ex. 132.123,-32.234)
+ * @returns
+ */
 async function getWeather(location, coords = null) {
+    let request = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&days=3`;
+
+    if (coords) {
+        request += `&q=${coords}`;
+    } else {
+        request += `&q=${location}`;
+    }
+
     try {
-        let requestString =
-            'https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&';
-
-        if (coords) {
-            requestString += `q=${coords}&days=3`;
-        } else {
-            requestString += `q=${location}&days=3`;
-        }
-
-        const response = await fetch(requestString);
-
-        return await response.json();
+        return await fetch(request);
     } catch (e) {
         console.error(e);
     }
 }
 
 async function getUserCoords() {
-    try {
-        if (!('geolocation' in navigator)) {
-            throw new Error('No Geolocation in Navigator');
-        }
+    const promise = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 
-        return await navigator.geolocation.getCurrentPosition((data) => {
-            return `${data.coords.longitude},${data.coords.latitude}`;
-        });
+    try {
+        const data = await promise;
+        return `${data.coords.latitude},${data.coords.longitude}`;
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+async function startUp() {
+    try {
+        const coords = await getUserCoords();
+        const weatherData = await getWeather(null, coords);
+        const data = await weatherData.json();
+        return data;
     } catch (e) {
         console.error(e);
-        return null;
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const coords = getUserCoords();
-
-    if (!coords) {
-        console.log('no');
-    } else {
-        console.log(coords);
-    }
+    startUp().then((data) => {
+        console.log(data);
+    });
 });
