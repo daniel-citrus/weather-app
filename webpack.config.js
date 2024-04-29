ef8d764c206e13af6fdc49308aa9147eb50e2fa2
Daniel <dandcalvo@gmail.com>
2024-04-29 08:48:42 -0700

    no more FOUC

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,46 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const { argv } = require('process');
 
-module.exports = {
-    entry: {
-        icons: './src/scripts/icons.js',
-        index: './src/scripts/index.js',
-        data: './src/scripts/dataExtractor.js',
-        display: './src/scripts/display.js',
-        weather: './src/scripts/weather.js',
-    },
-    output: {
-        clean: true,
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Weather Application',
-            template: './src/template.html',
-        }),
-        new MiniCssExtractPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
-                type: 'asset/resource',
-            },
+module.exports = (env, argv) => {
+    let prod = argv.mode === 'production';
+
+    return {
+        entry: {
+            icons: './src/scripts/icons.js',
+            index: './src/scripts/index.js',
+            data: './src/scripts/dataExtractor.js',
+            display: './src/scripts/display.js',
+            weather: './src/scripts/weather.js',
+        },
+        output: {
+            clean: true,
+            path: path.resolve(__dirname, './dist'),
+            filename: '[name].bundle.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Weather Application',
+                template: './src/template.html',
+            }),
+            new MiniCssExtractPlugin(),
         ],
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        prod ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+    };
 };
